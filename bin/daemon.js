b4214468333f6acd94ac26b447bc0a41faa9797d
Richard Lindner <richard.lindner@sap.com>
2021-02-17 21:19:39 +0100

    rollback from daemon disaster

--- bin/daemon.js
+++ bin/daemon.js
@@ -21,46 +21,18 @@ const _kill = (pid) => {
   }
 }
 
-const _status = (pid) => {
-  if (Number.isFinite(pid)) {
-    const statusCommand = `ps -p ${pid}`
-    execSync(statusCommand, { stdio: "pipe" })
-  } else {
-    throw new Error()
-  }
-}
-
 ;(async () => {
-  const [mode] = process.argv.slice(2);
-  if (!mode || !(["start", "status", "stop"].includes(mode))) {
-    console.log("webmonitor start|status|stop");
-    return;
-  }
+  try {
+    const pid = parseFloat((await readFile(PID_FILE)).toString())
+    _kill(pid)
+    // eslint-disable-next-line no-empty
+  } catch (err) {}
 
-  switch (mode) {
-    case "start": {
-      const foutlog = await open(LOG_FILE, "w")
-      const child = spawn(process.execPath, [SCRIPT_FILE].concat(process.argv.slice(2)), {
-        stdio: ["ignore", foutlog, foutlog],
-        detached: true,
-      })
-      await writeFile(PID_FILE, String(child.pid))
-      child.unref()
-    } break;
-    case "status": {
-      try {
-        const pid = parseFloat((await readFile(PID_FILE)).toString())
-        _status(pid)
-      } catch (err) {
-        console.log("webmonitor is not running")
-      }
-    } break;
-    case "stop": {
-      try {
-        const pid = parseFloat((await readFile(PID_FILE)).toString())
-        _kill(pid)
-        // eslint-disable-next-line no-empty
-      } catch (err) {}
-    } break;
-  }
+  const foutlog = await open(LOG_FILE, "w")
+  const child = spawn(process.execPath, [SCRIPT_FILE].concat(process.argv.slice(2)), {
+    stdio: ["ignore", foutlog, foutlog],
+    detached: true,
+  })
+  await writeFile(PID_FILE, String(child.pid))
+  child.unref()
 })()
